a0b615ab9269df88d11cf20ed45a783013c140a9
Mohamed-Hamdhi <144825750+Mohamed-Hamdhi@users.noreply.github.com>
2023-09-22 09:23:33 +0100

    Update codingroomtasks.py

--- codingroomtasks.py
+++ codingroomtasks.py
@@ -1,1 +1,12 @@
+#apple sharing
+students = int(input("Enter the Number of Students :"))
+apples = int(input("Enter the Number of Apples :"))
 
+remain = apples%students
+numapples = apples//students
+
+print("Apples that Each Student Have :",numapples)
+print("Apples reamining :",remain)
+
+
+#area of triangle
