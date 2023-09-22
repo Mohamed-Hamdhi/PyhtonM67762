747b3a95e89af31a8f01d9d7c9cb2590aaf7f880
Mohamed-Hamdhi <144825750+Mohamed-Hamdhi@users.noreply.github.com>
2023-09-22 09:24:27 +0100

    Update codingroomtasks.py

--- codingroomtasks.py
+++ codingroomtasks.py
@@ -10,3 +10,8 @@ print("Apples reamining :",remain)
 
 
 #area of triangle
+b = int(input("Enter the Base of the triangle :"))
+h = int(input("Enter the Height of the triangle :"))
+
+# Print the result with print()
+print("The Area of the Triangle is :",0.5*b*h)
