c51798c6ecb7ca3d91e9fba0d6650fd7aedd3344
Mohamed-Hamdhi <144825750+Mohamed-Hamdhi@users.noreply.github.com>
2024-01-13 15:58:50 +0000

    commit

--- areaoftriangle.py
+++
@@ -1,6 +0,0 @@
-# Read the numbers b and h like this:
-b = int(input("Enter the Base of the triangle :"))
-h = int(input("Enter the Height of the triangle :"))
-
-# Print the result with print()
-print("The Area of the Triangle is :",0.5*b*h)
